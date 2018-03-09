33b95243150844cadf44304ddf2cb29b8540f0d9
Graeme Erickson <graemeerickson@gmail.com>
2018-03-08 23:42:50 -0800

    Implemented array containing an object per card

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-var cards = ['queen','queen','king','king'];
+// define cards array containing an object for each card
+var cards = [
+   {
+      rank: "queen",
+      suit: "hearts",
+      cardImage: "images/queen-of-hearts.png"
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png"
+   },
+   {
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png"
+   },
+   {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png"
+   }
+];
+
 var cardsInPlay = [];
 
+// logic to check whether the two cards match
 var checkForMatch = function () {
-   if (cardsInPlay[0] === cardsInPlay[1]) {
+   if (cardsInPlay.length < 2) {
+      console.log("Waiting for second card to flip...")
+   } else if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
+      alert("You found a match!");
    } else {
       console.log("Sorry, try again.");
+      alert("Sorry, try again.");
    }
 }
 
+// receive flipped card, add it to cards in play, and check for a match
 var flipCard = function (cardId) {
-   console.log("User flipped " + cards[cardId]);
-   cardsInPlay.push(cards[cardId]);
+   console.log("User flipped " + cards[cardId].rank);
+   cardsInPlay.push(cards[cardId].rank);
+   console.log("Flipped card suit: " + cards[cardId].suit);
+   console.log("Flipped card image: " + cards[cardId].cardImage);
    checkForMatch();
 }
 
+// initiate card flipping
 flipCard(0);
 flipCard(2);
