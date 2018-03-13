8660133cc107ca8425315ffb64b75038b3e79da5
Graeme Erickson <graemeerickson@gmail.com>
2018-03-12 19:50:51 -0700

    Implemented DOM-manipulating JS to set the board, listen for a click, and store clicked cards in memory

--- js/main.js
+++ js/main.js
@@ -23,29 +23,43 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var cardId;  //global variable in order to reference in more than one function
 
 // logic to check whether the two cards match
 var checkForMatch = function () {
-   if (cardsInPlay.length < 2) {
-      console.log("Waiting for second card to flip...")
-   } else if (cardsInPlay[0] === cardsInPlay[1]) {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
-      alert("You found a match!");
+      setTimeout(function() { alert("You found a match!"); }, 25);
    } else {
       console.log("Sorry, try again.");
-      alert("Sorry, try again.");
-   }
-}
+      setTimeout(function() { alert("Sorry, try again."); }, 25);
+   };
+};
 
 // receive flipped card, add it to cards in play, and check for a match
-var flipCard = function (cardId) {
-   console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+   cardId = this.getAttribute('data-id');
    cardsInPlay.push(cards[cardId].rank);
+   console.log("User flipped " + cards[cardId].rank);
    console.log("Flipped card suit: " + cards[cardId].suit);
    console.log("Flipped card image: " + cards[cardId].cardImage);
-   checkForMatch();
-}
+   this.setAttribute('src',cards[cardId].cardImage);
+   if (cardsInPlay.length < 2) {
+      console.log("Waiting for second card to flip...")
+   } else {
+      checkForMatch()
+   };
+};
 
-// initiate card flipping
-flipCard(0);
-flipCard(2);
+// set up the board - four cards facing down
+var createBoard = function () {
+   for (var i = 0; i < cards.length; i++) {
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src','images/back.png');
+      cardElement.setAttribute('data-id',i);
+      document.getElementById('game-board').appendChild(cardElement);
+      cardElement.addEventListener('click',flipCard);
+   };
+};
+
+createBoard();
